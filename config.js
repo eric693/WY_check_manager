8522c1fe0dcc2ad80868a0092eb12145658c11aa
eric693 <exo881222@gmail.com>
2026-01-10 09:28:07 +0800

    update

--- config.js
+++ config.js
@@ -2,10 +2,10 @@
 
 const API_CONFIG = {
   // 正式環境的 API URL
-  apiUrl: "https://script.google.com/macros/s/AKfycbwIbjgqYKO1ahlU65u36FSblSC5PKIxwkC9lKRVIYvRnO7wwMc-Thm1CUAs1K5XO0jD/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbx9zrD8XCodkNFTiU__bnIVvozzOiQ1xHIUg77Vbw85MFPZYcjB6RCoVPELzBW3eNi4/exec",
   
   // 新增回呼網址
-  redirectUrl: "https://eric693.github.io/check_manager_plus/"
+  redirectUrl: "https://eric693.github.io/WY_check_manager/"
   // 你也可以在這裡加入其他設定，例如：
   // timeout: 5000,
   // version: 'v4.2.9'
